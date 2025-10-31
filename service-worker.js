af1cbb74ea324467825a546a33541b170e0390c2
Gabriel26200 <gabriel.puaux@gmail.com>
2025-10-31 16:21:24 +0100

    Update service-worker.js

--- service-worker.js
+++ service-worker.js
@@ -2,18 +2,18 @@
 "name": "Carnet d'Entraînement Numérique",
 "short_name": "Carnet Entraînement",
 "description": "Suivi numérique de votre progression d'entraînement.",
-"start_url": "/Carnet-d-entrainement/index.html",
+"start_url": "/Carnet-d-entrainement/",
 "display": "standalone",
 "background_color": "#F8F9FA",
 "theme_color": "#2563EB",
 "icons": [
 {
-"src": "https://www.google.com/search?q=https://placehold.co/192x192/2563EB/ffffff%3Ftext%3DCE",
+"src": "/Carnet-d-entrainement/https://www.google.com/search?q=https://placehold.co/192x192/2563EB/ffffff%3Ftext%3DCE",
 "sizes": "192x192",
 "type": "image/png"
 },
 {
-"src": "https://www.google.com/search?q=https://placehold.co/512x512/2563EB/ffffff%3Ftext%3DCE",
+"src": "/Carnet-d-entrainement/https://www.google.com/search?q=https://placehold.co/512x512/2563EB/ffffff%3Ftext%3DCE",
 "sizes": "512x512",
 "type": "image/png"
 }
